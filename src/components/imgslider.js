0ef34fc1d14901dc5d31c7e36e043c705a804286
sothearak <hengsothearakgt@gmail.com>
2023-08-27 04:18:25 +1000

    V@3

--- src/components/imgslider.js
+++ src/components/imgslider.js
@@ -1,21 +1,67 @@
-import { useState} from 'react';
+import { useState } from 'react';
 
 
 const ImageSlider = ({ slides }) => {
-	const [currentIndex, setcurrentuser] = useState(0);
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+
+	const sliderstyles = {
+
+		height: "100%",
+		position:"relative"
+	};
 
 	const slidestyles = {
-		width: '100%',
+		width: "100%",
+		height: "100%",
+		borderRadius: "10px",
+		backgroundPosition: "center",
+		backgroundSize: "cover",
+		backgroundImage: `url(${slides[currentIndex].url})`,
+	};
 
-	}
+	const leftArrowStyles = {
+		position: "absolute",
+		top: "50%",
+		transform: "translate(0, -50%)",
+		left: "32px",
+		fontSize: "45px",
+		color: "#fff",
+		zIndex: 1,
+		cursor: "pointer",
 
+	};
+
+	const rightArrowStyles = {
+		position: "absolute",
+		top: "50%",
+		transform: "translate(0, -50%)",
+		right: "32px",
+		fontSize: "45px",
+		color: "#fff",
+		zIndex: 1,
+		cursor: "pointer",
+
+	};
+
+	const goToPrevious = () => {
+		const isFirstSlide = currentIndex === 0
+		const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+		setCurrentIndex(newIndex);
+	};
+
+	const goToNext = () => {
+		const isLastSlide = currentIndex === slides.length - 1;
+		const newIndex = isLastSlide ? 0 : currentIndex + 1;
+		setCurrentIndex(newIndex);
+	};
 	return (
-		<div>
-			<div
-				style={{ backgroundImage: 'url(${slides[currentIndex].url})' }}
-			></div>
+		<div style={sliderstyles}>
+			<div style={leftArrowStyles} onClick={goToPrevious}>(</div>
+			<div style={rightArrowStyles} onClick={goToNext}>)</div>
+			<div style={slidestyles}></div>
 		</div>
-	)
+	);
 };
 
 export default ImageSlider;
--- src/home.js
+++ src/home.js
@@ -19,19 +19,19 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const slides = [
-    { url: 'https://images.unsplash.com/photo-1609258612794-3502fb4306a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80', title: 'yoda' },
-    { url: 'https://images.unsplash.com/photo-1610714872435-96712ebf1594?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2654&q=80', title: 'yoda' },
-    { url: 'https://images.unsplash.com/photo-1618336753974-aae8e04506aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80', title: 'yoda' },
-    { url: 'https://images.unsplash.com/photo-1664202925603-1d2bf7f190fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80', title: 'yoda' },
-    { url: 'https://images.unsplash.com/photo-1657333813883-8da3bc1e2c07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2672&q=80', title: 'yoda' }
+    { url: "https://images.unsplash.com/photo-1609258612794-3502fb4306a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80", title: "yoda1" },
+    { url: "https://images.unsplash.com/photo-1610714872435-96712ebf1594?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2654&q=80", title: "yoda2" },
+    { url: "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80", title: "yoda3" },
+    { url: "https://images.unsplash.com/photo-1664202925603-1d2bf7f190fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80", title: "yoda4" },
+    { url: "https://images.unsplash.com/photo-1657333813883-8da3bc1e2c07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2672&q=80", title: "yoda5" }
 
 
-]
+];
 const containerStyles = {
-    width: "500px",
-    height: "280px",
+    width: "100%",
+    height: "700px",
     margin: "0 auto "
-}
+};
 
 
 export default function BasicGrid() {
@@ -46,12 +46,15 @@ export default function BasicGrid() {
                 <Grid item xs={12} sm={12} md={12}>
                     <Item>
                         <div style={containerStyles}>
-                        < ImageSlider slides={slides} />
+                            < ImageSlider slides={slides} />
+
                         </div>
                     </Item>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Item>grid 1</Item>
+                    <Item>
+
+                        grid 1</Item>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     <Item>Grid 2</Item>
