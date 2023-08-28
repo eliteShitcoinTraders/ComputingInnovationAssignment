c9d5e9042acccfdbf5707882bf3b2ea3dcec1bf2
henrys <110834511+HenryNgu1@users.noreply.github.com>
2023-08-28 14:44:21 +1000

    Links to another page
    added, product page, install react route thing updated ap..js to include route to product page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import BasicGrid from './home.js'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary components from React Router
+import BasicGrid from './home.js';
+import Product from './product'; // Import the component for the product page
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <BasicGrid />
-                
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div>
+            <Router>  
+                <Routes>
+                    <Route path="/" element={<BasicGrid />} /> 
+                    <Route path="/product" element={<Product />} /> 
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
