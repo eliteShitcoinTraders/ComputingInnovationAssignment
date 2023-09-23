75eae8484299c2396a53696b8413da7501643f38
henroidss <henrynguyen12357@gmail.com>
2023-09-23 17:50:58 +1000

    Revert "test roll back"
    This reverts commit e8f33e1f962d8b0c1befab188843f358b37e6639.

--- src/components/inputfield.js
+++ src/components/inputfield.js
@@ -14,26 +14,26 @@ import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import MenuItem from '@mui/material/MenuItem';
-
+//drop down list values
 const currencies = [
     {
-      value: 'USD',
-      label: '$ - USD',
+      value: 'ETH',
+      label: 'ETH',
     },
     {
       value: 'BTC',
-      label: '฿ - BTC',
+      label: 'BTC',
     },
     {
       value: 'AUD',
-      label: '$ - AUD',
+      label: 'AUD',
     },
   ];
 
 export default function Inputs() {/*MUI inputs where we use for the user name and password*/
   const [showPassword, setShowPassword] = React.useState(false);
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowPassword = () => setShowPassword((show) => !show); //hide and show password functions
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
@@ -43,14 +43,14 @@ export default function Inputs() {/*MUI inputs where we use for the user name an
     <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
       <div>
       <FormControl sx={{ m: 1, width: '30ch'}} variant="standard">
-          <InputLabel htmlFor="username">Username</InputLabel>
+          <InputLabel htmlFor="username">Username</InputLabel> {/*input for username section*/}
           <Input
             id="username"
           />
         </FormControl>
 
         <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
-          <InputLabel htmlFor="pw">Password</InputLabel>
+          <InputLabel htmlFor="pw">Password</InputLabel> {/*input for pw*/}
           <Input
             id="pw"
             type={showPassword ? 'text' : 'password'}
@@ -73,12 +73,12 @@ export default function Inputs() {/*MUI inputs where we use for the user name an
       </div>
       <div>
         <TextField
-          label="Wallet ID"
+          label="Wallet ID" //input for wallet id
           id="walletID"
           sx={{ m: 1, width: '50ch' }}
           variant="standard"
         />
-        <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
+        <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined"> {/* drop down options for currency*/}
         <TextField
           id="outlined-select-currency"
           select
